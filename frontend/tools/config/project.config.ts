1c365389b7159d33ca28ade0bc64e25da96e5ff7
Ole Spaarmann <spaarmann@soerlaf.com>
2017-02-16 00:38:07 +0200

    Initial commit frontend

---
+++ frontend/tools/config/project.config.ts
@@ -0,0 +1,53 @@
+import { join } from 'path';
+
+import { SeedConfig } from './seed.config';
+// import { ExtendPackages } from './seed.config.interfaces';
+
+/**
+ * This class extends the basic seed configuration, allowing for project specific overrides. A few examples can be found
+ * below.
+ */
+export class ProjectConfig extends SeedConfig {
+
+  PROJECT_TASKS_DIR = join(process.cwd(), this.TOOLS_DIR, 'tasks', 'project');
+
+  constructor() {
+    super();
+    // this.APP_TITLE = 'Put name of your app here';
+    // this.GOOGLE_ANALYTICS_ID = 'Your site's ID';
+
+    /* Enable typeless compiler runs (faster) between typed compiler runs. */
+    // this.TYPED_COMPILE_INTERVAL = 5;
+
+    // Add `NPM` third-party libraries to be injected/bundled.
+    this.NPM_DEPENDENCIES = [
+      ...this.NPM_DEPENDENCIES,
+      // {src: 'jquery/dist/jquery.min.js', inject: 'libs'},
+      // {src: 'lodash/lodash.min.js', inject: 'libs'},
+    ];
+
+    // Add `local` third-party libraries to be injected/bundled.
+    this.APP_ASSETS = [
+      // {src: `${this.APP_SRC}/your-path-to-lib/libs/jquery-ui.js`, inject: true, vendor: false}
+      // {src: `${this.CSS_SRC}/path-to-lib/test-lib.css`, inject: true, vendor: false},
+    ];
+
+    // Add packages (e.g. ng2-translate)
+    // let additionalPackages: ExtendPackages[] = [{
+    //   name: 'ng2-translate',
+    //   // Path to the package's bundle
+    //   path: 'node_modules/ng2-translate/bundles/ng2-translate.umd.js'
+    // }];
+    //
+    // this.addPackagesBundles(additionalPackages);
+
+    /* Add proxy middlewar */
+    // this.PROXY_MIDDLEWARE = [
+    //   require('http-proxy-middleware')({ ws: false, target: 'http://localhost:3003' })
+    // ];
+
+    /* Add to or override NPM module configurations: */
+    // this.PLUGIN_CONFIGS['browser-sync'] = { ghostMode: false };
+  }
+
+}
