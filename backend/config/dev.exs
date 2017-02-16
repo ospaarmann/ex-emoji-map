34758c719e24d002e77a474d2e83675f6588f3fb
Ole Spaarmann <spaarmann@soerlaf.com>
2017-02-17 00:40:17 +0200

    Update check origins value in backend

--- backend/config/dev.exs
+++ backend/config/dev.exs
@@ -10,7 +10,7 @@ config :emoji_map, EmojiMap.Endpoint,
   http: [port: 4000],
   debug_errors: true,
   code_reloader: true,
-  check_origin: ["//localhost", "//localhost:5555"],
+  check_origin: ["//localhost"],
   watchers: []
 
 
