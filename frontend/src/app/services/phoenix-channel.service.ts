67e168ebac8cf51336b3dc66165d11f9116fc48f
Ole Spaarmann <spaarmann@soerlaf.com>
2017-02-23 09:20:50 +0100

    Display error or success when joining channel

--- frontend/src/app/services/phoenix-channel.service.ts
+++ frontend/src/app/services/phoenix-channel.service.ts
@@ -9,12 +9,15 @@ import { Tweet } from '../models/tweet.model';
 @Injectable()
 export class PhoenixChannelService {
 
-  socket:any;
-  channel:any;
+  public socket:any;
+  public channel:any;
+  public alerts:Array<any>;
 
-  constructor(private mapService:MapService) { }
+  constructor(private mapService:MapService) {
+    this.alerts = [];
+  }
 
-  connect() {
+  connect():void {
     this.socket = new Phoenix.Socket(environment.socket_endpoint + '/map_socket', {
       //logger: (kind, msg, data) => { console.log('%s: %s', kind, msg, data) }
     });
@@ -23,16 +26,42 @@ export class PhoenixChannelService {
 
     this.channel = this.socket.channel('map:updates');
     this.channel.join()
-      .receive("ok", resp => { console.log("Joined successfully", resp) })
-      .receive("error", resp => { console.log("Unable to join", resp) });
-    this.channel.onError(e => console.log('something went wrong', e));
-    this.channel.onClose(e => console.log('channel closed', e));
+      .receive("ok", resp => {
+        this.displaySuccess("Joined channel successfully");
+        console.log("Joined successfully", resp)
+      })
+      .receive("error", resp => {
+        this.displayError('Error joining channel.');
+        console.log("Unable to join", resp);
+      });
+    this.channel.onError(e => {
+      this.displayError('Error joining channel.');
+      console.log('something went wrong', e);
+    });
+    this.channel.onClose(e => {
+      this.displayError('Channel closed.');
+      console.log('channel closed', e)
+    });
 
     this.channel.on('new:msg', msg => {
       let tweet = new Tweet(msg);
       // Draw it on the map
       this.mapService.addTweet(tweet);
     });
+  }
+
+  displayError(msg:string):void {
+    this.alerts = [{
+      type: 'danger',
+      msg: msg
+    }];
+  }
+
+  displaySuccess(msg:string):void {
+    this.alerts = [{
+      type: 'success',
+      msg: msg
+    }];
   }
 
 }
